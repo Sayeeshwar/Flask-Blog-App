f57eaa6f0db92cba2c03ff6c2848aa77c3dd984e
Sayeeshwar <sayeeshwar.girish@gmail.com>
2020-05-03 16:18:29 +0530

    Add dummy posts data and pass to posts.html

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+
+all_posts=[
+    {
+        'title':'First Post',
+        'content':'Hello world!',
+        'author': 'Sai'
+    },
+    {
+        'title':'2nd Post',
+        'content':'Hello Universe!',
+        'author': 'Chandran'
+    }
+]
+
+
 @app.route('/')
 def hello():
     return render_template('index.html')
 
 @app.route('/posts')
 def posts():
-    return render_template('posts.html')
+    return render_template('posts.html',posts=all_posts)
 
-    
+
 if __name__=="__main__":
     app.run(debug=True)
